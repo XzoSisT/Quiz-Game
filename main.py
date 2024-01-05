0f99b8e519668d5a2046c168830373039cd2bbd1
XzoSisT <phat4725@gmail.com>
2024-01-05 23:56:06 +0700

    เพิ่มmoduleและlibraryที่คิดว่าจำเป็น

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.label import Label
+from kivy.uix.button import Button
+from kivy.uix.popup import Popup
+import random
 
 class QuizGame(App):
     def build(self):
-        pass
+        self.layout = BoxLayout(orientation='vertical', spacing=10)
+        self.question_label = Label(text="")
 
 if __name__ == '__main__':
     QuizGame().run()
